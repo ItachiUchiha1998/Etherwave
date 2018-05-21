f2e1a3cb1a68861a38ffad1f9e365655920cf6d3
ItachiUchiha1998 <vinayak.shrivastava@gmail.com>
2018-05-21 13:08:25 +0530

    qr code

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,11 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
 
-router.get('/', (req, res, next) => {
-  res.render('login');
+router.get('/qr', (req, res, next) => {
+  res.render('qr');
 });
 
-router.get('/home', (req,res,next) => {
+router.get('/', (req,res,next) => {
   
   db.Ticket.find({}, (err, ticket) => {
         if (err) {
