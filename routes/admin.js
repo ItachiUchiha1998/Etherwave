cd4f2b4abfb1e594c79eaa5a0d2e196a6f8d28f0
ItachiUchiha1998 <vinayak.shrivastava@gmail.com>
2018-05-25 03:40:03 +0530

    purchased ticket page  added

--- routes/admin.js
+++ routes/admin.js
@@ -8,5 +8,9 @@ router.get('/', (req,res,next) => {
 router.get('/qr', (req, res, next) => {
     res.render('qr');
 });
+
+router.get('/ticket/:id',(req,res) => {
+	res.render('ticket',{id: req.params.id});
+})
   
 module.exports = router;
